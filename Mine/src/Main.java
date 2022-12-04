0d0e88e8d2cfe8972a4e902af87bd4af05f1ec3e
Rafal Szachowski <evkszach@gmail.com>
2022-12-04 14:36:13 +0100

    console ui, managing warehouse functions, transferring resources to stack

--- Mine/src/Main.java
+++ Mine/src/Main.java
@@ -1,21 +1,60 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Mine mine = new Mine("First mine");
+        Mine mine1 = new Mine("First mine");
+        consoleUI(mine1);
+    }
 
-        for(int i = 0; i<15; i++){
-            mine.goMining();
+    private static void consoleUI(Mine mine){
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        uiMenu();
+        while(!quit){
+            int action = scanner.nextInt();
+            scanner.nextLine();
+            switch (action){
+                case 0:
+                    System.out.println("Quitting the app...");
+                    quit = true;
+                    break;
+                case 1:
+                    mine.goMining();
+                    break;
+                case 2:
+                    mine.passTime();
+                    break;
+                case 3:
+                    mine.manageWarehouse();
+                    uiMenu();
+                    break;
+                case 4:
+                    System.out.println("Rename to: ");
+                    String newName = scanner.nextLine();
+                    mine.setName(newName);
+                    System.out.println("Mine has been renamed to " + mine.getName());
+                    break;
+                case 5:
+                    System.out.println(mine.getPassedDays() + " days have passed");
+                    break;
+                case 6:
+                    System.out.println(mine.getName());
+                    break;
+                case 7:
+                    uiMenu();
+            }
         }
+    }
 
-        mine.manageWarehouse();
-        mine.passTime();
-
-        for(int i = 0; i<15; i++){
-            mine.goMining();
-        }
-        mine.manageWarehouse();
-
-        
-
+    private static void uiMenu(){
+        System.out.println("0 - quit the app \n" +
+                "1 - go mining \n" +
+                "2 - pass time \n" +
+                "3 - manage the warehouse \n" +
+                "4 - rename the mine  \n" +
+                "5 - show passed days \n" +
+                "6 - display mine details \n"+
+                "7 - display possible actions");
     }
 }
--- Mine/src/Mine.java
+++ Mine/src/Mine.java
@@ -1,9 +1,9 @@
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Stack;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Mine implements IMine{
+
 
     private ArrayList<Resources> mineWarehouse = new ArrayList<Resources>();
     public Stack<Resources> mineStockpile = new Stack<Resources>();
@@ -12,11 +12,22 @@ public class Mine implements IMine{
     private int workingHours = 0;
     private int passedDays = 0;
 
+
     public Mine(String name) {
         this.name = name;
     }
 
+    public int getPassedDays() {
+        return passedDays;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     @Override
     public void goMining() {
@@ -51,31 +62,85 @@ public class Mine implements IMine{
         }else{
             mineWarehouse.add(result);
         }
-
-
     }
 
     public void passTime(){
+        System.out.println("Time has passed...");
         this.workingHours=0;
         passedDays++;
     }
 
     @Override
     public void manageWarehouse() {
-        System.out.println("Warehouse contains: ");
-        for(Resources res : mineWarehouse){
-            System.out.println(res.name());
+        ListIterator<Resources> listIterator = mineWarehouse.listIterator();
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        System.out.println(warehouseMenu());
+        System.out.println("Choose action to perform: ");
+
+        while(!quit){
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(choice){
+                case 0:
+                    System.out.println("Leaving the warehouse...");
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.println("Warehouse contains: ");
+                    for(Resources res : mineWarehouse){
+                        System.out.println(res.name());
+                    }
+                    break;
+                case 2:
+                    if(listIterator.hasNext()){
+                        System.out.println("Selected: " + listIterator.next());
+                    }else{
+                        System.out.println("You've reached end of the warehouse.");
+                    }
+                    break;
+                case 3:
+                    if(listIterator.hasPrevious()){
+                        System.out.println("Selected: "+ listIterator.previous());
+                    }else{
+                        System.out.println("You are now at the front of the warehouse.");
+                    }
+                    break;
+                case 4:
+                    mineStockpile.push(listIterator.previous());
+                    listIterator.remove();
+                    System.out.println("Moved to the stockpile");
+                    break;
+                case 5:
+                    manageStockpile();
+                    break;
+                case 6:
+                    System.out.println(warehouseMenu());
+            }
         }
     }
 
     @Override
     public void manageStockpile() {
-
+        System.out.println("Things in the stockpile: "+ mineStockpile.toString());
+       // mineStockpile.forEach(System.out::println);
     }
 
     @Override
     public boolean chainCrafting() {
         return false;
+    }
+
+    public String warehouseMenu(){
+        return "Possible actions: \n" +
+                "0 - exit the management \n" +
+                "1 - display contents of the warehouse \n" +
+                "2 - select next resource \n" +
+                "3 - select previous resource \n" +
+                "4 - move resource to the stockpile \n" +
+                "5 - show stockpile's contents (temporary option) \n" +
+                "6 - display possible actions";
     }
 
 }
